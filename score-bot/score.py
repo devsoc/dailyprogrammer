bcf9a9f31cdd7ae91723cfeb8d1e25ec88fbbf41
arsh23 <programmer.arsh@gmail.com>
2017-02-16 22:54:15 +0530

    got readme update working

--- score-bot/score.py
+++ score-bot/score.py
@@ -1,9 +1,13 @@
 import re
+import json
+import base64
 from collections import Counter
 
 import aiohttp
 import asyncio
 import async_timeout
+
+import settings
 
 
 async def get(session, url, loop):
@@ -12,23 +16,53 @@ async def get(session, url, loop):
             return await response.json()
 
 
-async def get_names(loop, baseurl):
+async def get_names(session, loop, baseurl):
     files = list()
-    async with aiohttp.ClientSession(loop=loop) as session:
-        for x in await get(session, baseurl, loop):
-            resp = await get(session, x['git_url'] + '?recursive=1', loop)
-            files += [x['path'] for x in resp['tree']]
+    for x in await get(session, baseurl, loop):
+        resp = await get(session, x['git_url'] + '?recursive=1', loop)
+        files += [x['path'] for x in resp['tree']]
     names = Counter(re.findall(r"-(\w+).", x)[0] for x in files if '-' in x)
     return names.most_common()
 
 
-def main():
+async def update_readme(session, scores, loop, api_url):
+    url = api_url + '/repos/devsoc/dailyprogrammer/contents/README.md'
+    resp = await get(session, url, loop)
+    sha = resp['sha']
+
+    new_readme = list()
+    print(scores)
+    readme = base64.b64decode(resp['content']).decode()
+    for line in readme.split('\n'):
+        new_readme.append(line)
+        if '| ------------- |' in line:
+            new_readme += ['| ' + n + ' | ' + str(s) + ' |' for n, s in scores]
+    new_readme = '\n'.join(new_readme)
+    print(new_readme)
+
+    commit = {
+        'committer': {'name': 'Bot', 'email': 'programmer.arsh@gmail.com'},
+        'sha': sha,
+        'message': '[Bot commit] testing api',
+        'content': base64.b64encode(new_readme.encode()).decode()
+    }
+    print(commit)
+
+    print(url)
+    url += '?access_token=' + settings.token
+    async with session.put(url, data=json.dumps(commit)) as resp:
+        r = await resp.json()
+    print(r)
+
+
+async def main(loop):
     api_url = 'https://api.github.com'
     url = api_url + '/repos/devsoc/dailyprogrammer/contents/solutions'
-    loop = asyncio.get_event_loop()
-    return loop.run_until_complete(get_names(loop, url))
+    async with aiohttp.ClientSession(loop=loop) as session:
+        scores = await get_names(session, loop, url)
+        return await update_readme(session, scores, loop, api_url)
 
 
 if __name__ == '__main__':
-    names = main()
-    print(names)
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main(loop))
